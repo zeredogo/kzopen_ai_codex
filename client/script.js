6641821fd17ad7b872fbbd611aa492eb3ffba270
zeredogo <zeredogo@gmail.com>
2023-01-14 15:55:10 +0100

    change made

--- client/script.js
+++ client/script.js
@@ -2,3 +2,6 @@ import bot from './assets/bog.svg';
 import user from './assets/user.svg';
 
 const form = document.querySelector('from');
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
