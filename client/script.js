1e692df7614ebef430396ed3fbdac4f0470fa51d
zeredogo <zeredogo@gmail.com>
2023-01-15 05:14:01 +1400

    change made

--- client/script.js
+++ client/script.js
@@ -5,3 +5,16 @@ const form = document.querySelector('from');
 const chatContainer = document.querySelector('#chat_container');
 
 let loadInterval;
+
+function loader(element) {
+  element.textContent = '';
+
+  loadInterval = setInterval(() => {
+    element.textContent += '.';
+
+    if (element.textContent === '....') {
+      element.textContent = '';
+    }
+  }, 300)
+
+}
