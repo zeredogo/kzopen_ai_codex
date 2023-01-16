978b18221323878e07c6fec55648871c1dc1fa92
zeredogo <zeredogo@gmail.com>
2023-01-17 04:48:36 +1400

    update  made

--- client/script.js
+++ client/script.js
@@ -81,5 +81,7 @@ const handleSubmit = async (e) => {
 
 form.addEventListener('submit', handleSubmit);
 form.addEventListener('keyup', (e) => {
-  if (e.keyCode === 13)
+  if (e.keyCode === 13) {
+    handleSubmit(e);
+  }
 })
